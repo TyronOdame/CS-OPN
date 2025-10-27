79c1eb028307ac4413bb77d7374a50e43d0f8632
Tyron <tyronodame5@gmail.com>
2025-10-26 23:00:54 -0400

    feat: implement user model methods for password handling and JSON serialization

--- backend/models/users.go
+++ backend/models/users.go
@@ -1,8 +1,7 @@
 package models
 
-import(
+import (
 	"time"
-
 
 	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
@@ -26,4 +25,44 @@ type User struct {
 }
 
 // function to handle pre-user creation
-func (u *User) 
+func (u *User) BeforeCreate(tx *gorm.DB) error {
+	if u.ID == uuid.Nil {
+		u.ID = uuid.New()
+	}
+	// Users start with 100 CBs
+	if u.Casebucks == 0 {
+		u.Casebucks = 100
+	}
+	return nil
+}
+
+// Hashing the users password before saving to the database
+func (u *User) HashPassword(password string) error {
+
+	// This is where bcrypt generates the hashed password with the users given password(the byte conversion is necessary)
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	u.Password = string(hashedPassword)
+	return nil
+}
+
+// Checking to see if the password given matches the hashed password stored in the db
+func (u *User) CheckPassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+	return err == nil
+}
+
+// This function returns a safe JSON version of a user without any of the sensitive information
+func (u *User) ToJSON() map[string]interface{} {
+	return map[string]interface{} {
+		"id":         u.ID,
+		"email":      u.Email,
+		"username":   u.Username,
+		"casebucks":  u.Casebucks,
+		"created_at": u.CreatedAt,
+		"updated_at": u.UpdatedAt,
+	}
+}
